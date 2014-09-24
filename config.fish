6d3a243c59e722d89e4541013753110d30098757
Joar Wandborg <joar@wandborg.se>
2014-09-24 11:10:05 +0200

    Fix

--- config.fish
+++ config.fish
@@ -3,4 +3,4 @@ set -e fish_greeting
 set -x __fish_config_dir (dirname (status -f))
 set -l __local_config $__fish_config_dir/config.local.fish
 
-test -f $__local_confi; and . $__local_config
+test -f $__local_config; and . $__local_config
