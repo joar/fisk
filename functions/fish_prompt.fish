ca0bf3c902cb697c97b01972fc5e27964d0224fb
Joar Wandborg <joar@wandborg.se>
2015-09-20 14:54:43 +0200

    Update fish_prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,51 +1,170 @@
 function fish_prompt --description 'Write out the prompt'
-    
-    set last_ret $status
-    # Just calculate these once, to save a few cycles when displaying the prompt
-    if not set -q __fish_prompt_hostname
-        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    set -g last_ret $status
+    if not set -q status
+        set -g last_ret 0
     end
 
-    if not set -q __fish_prompt_cwd
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+    # Colors
+    set -g c_return_decoration 777777
+    set -g c_return_error ff7777
+    set -g c_return_success 77ff77
+    set -g c_virtualenv ff00de
+    set -g c_cwd green
+    set -g c_git -o yellow
+    set -g c_user 5555ff
+    set -g c_user_delimiter 5555aa
+    set -g c_host $c_user
+    set -g c_prompt_end $c_user
+
+    # Configurable text
+    set -g t_user_delimiter "@"
+    set -g t_return_surround \
+        (concat (set_color $c_return_decoration) "[" \
+                (set_color normal)) \
+        (concat (set_color $c_return_decoration) "]" \
+                (set_color normal))
+    set -g t_prompt_end '➞'
+
+    # Private utility functions
+    # - Functions that could be executed more than once in fish_prompt.
+    # - Moke sure to delete all of these at the end of fish_prompt
+    function surround_text_with --argument-names text before after
+        if not set -q after
+            set -l after $before
+        end
+        concat $before $text $after
     end
 
-    if set -q VIRTUAL_ENV
-        set venv (echo -n -s " " (set_color ff00de) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal))
-    else
-        set venv ""
+    function join_with_str --argument-names str
+        set -l list
+        if test (count $argv) -eq 1
+            set list $argv[2]
+        else
+            set list $argv[2..-1]
+        end
+
+        set -l is_first 0
+
+        for item in $list
+            if not test "$is_first" -eq 0
+                concat $str
+            end
+
+            concat $item
+
+            if test "$is_first" -eq 0
+                set is_first 1
+            end
+        end
     end
 
-    git branch >  /dev/null ^ /dev/null
-    if [ $status = 0 ]
-        set branch (concat (set_color -o blue) "(" (basename (git rev-parse --show-toplevel)) ")")
-    else
-        set branch ""
+    function pad_str --argument-names str
+        switch "$str"
+            case ''
+            case ' *'
+                concat $str
+            case '*'
+                concat " " $str
+        end
     end
 
-    set ret_color 7f7
-
-    if [ $last_ret -gt 0 ]
-        set ret_color f77
+    # Private functions
+    function update_hostname
+        if not set -q __fish_prompt_hostname
+            set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+        end
     end
 
-    set -l ret_part (concat \
-        (set_color 777) '[' \
-        (set_color $ret_color) $last_ret \
-        (set_color 777) ']' \
-    )
+    function set_color_for_return_status --argument-names return_status
+        set -l color $c_return_success
+        if test "$return_status" -gt 0
+            set color $c_return_error
+        end
+        set_color $color
+    end
 
-    set -l user_part (concat \
-        (set_color 55f) $USER \
-        (set_color 55a) @ \
-        (set_color 55f) $__fish_prompt_hostname \
-    )
+    function get_text_venv
+        if set -q VIRTUAL_ENV
+            concat \
+                (set_color $c_virtualenv) \
+                "(" \
+                (basename "$VIRTUAL_ENV") \
+                ")" \
+                (set_color normal)
+        end
+    end
 
-    set -l _context_part (concat \
-        "$__fish_prompt_cwd" (prompt_pwd) "$venv" (set_color -o yellow) (__fish_git_prompt) \
-    )
+    function get_text_return_status
+        surround_text_with \
+            (concat (set_color_for_return_status $last_ret) $last_ret \
+                (set_color normal)) \
+            $t_return_surround
+    end
 
-    set -l prompt_end (concat (set_color 55f) '➞')
+    function get_text_user
+        join_with_str $t_user_delimiter \
+            (concat (set_color $c_user) $USER) \
+            (concat (set_color $c_host) $__fish_prompt_hostname \
+                (set_color normal))
+    end
 
-    echo -n (concat $ret_part " " $user_part " " $_context_part " " $prompt_end (set_color normal) " ")
+    function get_text_cwd
+        concat (set_color $c_cwd) (prompt_pwd) \
+                (set_color normal)
+    end
+
+    function get_text_git
+        set -l output (__fish_git_prompt ^ /dev/null)
+        if not test "$output" = ""
+            concat (set_color $c_git) $output \
+                (set_color normal)
+        end
+    end
+
+    function get_text_debug
+        set -l filename (status -f)
+
+        if not test "$filename" = "Standard input"
+            concat \
+                (set_color fff) $filename \
+                (set_color 777) ":" \
+                (set_color fff) (status -n) \
+                (set_color normal)
+        end
+    end
+
+    function get_text_prompt_end
+        concat (set_color $c_prompt_end) $t_prompt_end
+    end
+
+    # Assemble prompt
+    update_hostname
+
+    printf "%s %s %s%s %s %s" \
+        (get_text_return_status) \
+        (get_text_user) \
+        (get_text_cwd) \
+        (concat \
+            (pad_str (get_text_venv)) \
+            (pad_str (get_text_git)) \
+        ) \
+        (get_text_prompt_end)
+
+    printf (set_color normal)
+
+    # Remove private utility function
+    functions -e surround_text_with
+    functions -e join_with_str
+    functions -e pad_str
+    # Private functions
+    functions -e get_text_user
+    functions -e get_hostname
+    functions -e set_color_for_return_status
+    functions -e get_text_venv
+    functions -e get_text_return_status
+    functions -e get_text_cwd
+    functions -e set_text_branch
+    functions -e get_text_git
+    functions -e get_text_debug
+    functions -e get_text_prompt_end
 end
